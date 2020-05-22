b931afb66519b99fe0258baa4a99da4ba6e2a331
akane <mfmf.mfmf.mfmf.pu@gmail.com>
2020-05-22 17:02:25 +0900

    データバインディングできるように修正

--- Lesson2/2-4/AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Lesson2/2-4/AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,23 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // バインディングで呼び出せるようにする
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
         // ボタンタップの処理
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
         // TextViewタップの処理
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)
         }
     }
@@ -30,15 +37,17 @@ class MainActivity : AppCompatActivity() {
      * ニックネーム追加
      */
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
         // 入力した値をTextViewに挿入
-        nicknameTextView.text = editText.text
+//        binding.nicknameText.text = binding.nicknameEdit.text
 
-        // EditTextとButtonの非表示、TextViewの表示
-        editText.visibility = View.GONE
-        view.visibility = View.GONE     // ボタン
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            // EditTextとButtonの非表示、TextViewの表示
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // キーボード非表示にする
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -49,19 +58,16 @@ class MainActivity : AppCompatActivity() {
      * ニックネームの編集
      */
     private fun updateNickname (view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.done_button)
-
         //　EditTextとButtonの表示、TextViewの非表示
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE     // ニックネームのTextView
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE    // ニックネームのTextView
 
         // EditTextにフォーカス
-        editText.requestFocus()
+        binding.nicknameEdit.requestFocus()
         // キーボードの表示
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 
 }
